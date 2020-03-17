c76fa8b373b1fe52236fbf4ee1406410f909a298
Rylie Breslin <33608974+BreslinRy@users.noreply.github.com>
2020-03-16 18:24:05 -0700

    Add files via upload

---
+++ Count.py
@@ -0,0 +1,10 @@
+op1 = 0
+op2 = 0
+
+
+def count(option1, option2):
+    global op1, op2
+    op1 += option1
+    op2 += option2
+    print(op1)
+    print(op2)
--- GUI.py
+++ GUI.py
@@ -1,4 +1,7 @@
+#   Rylie Breslin
+#   SE420 Assignment 4
 from tkinter import *
+from Count import count
 
 
 class FullScreenApp(object):
@@ -20,12 +23,16 @@ class FullScreenApp(object):
     # Interface with a database here
 
 
-mgui = Tk()
-mgui.title('Vote')
-var1 = IntVar()
-Checkbutton(mgui, text='Option 1', variable=var1, padx=mgui.winfo_screenwidth()/2).grid(row=0, sticky=W)
-var2 = IntVar()
-Checkbutton(mgui, text='Option 2', variable=var2, padx=mgui.winfo_screenwidth()/2).grid(row=1, sticky=W)
+mgui = Tk()     # Setting tkinter equal to a callable name
+mgui.title('Vote')      # Setting the title for the GUI
+var1 = IntVar()     # Will allow us to tell if this option was checked
+Checkbutton(mgui, text='John Johnson', variable=var1, padx=mgui.winfo_screenwidth()/2).grid(row=0, sticky=W)
+# Creating a checkbox(tkinter name, candidate option, save state of box location, location on screen)
+var2 = IntVar()     # Will allow us to tell if this option was checked
+Checkbutton(mgui, text='Sam Samson', variable=var2, padx=mgui.winfo_screenwidth()/2).grid(row=1, sticky=W)
+# Creating a checkbox(tkinter name, candidate option, save state of box location, location on screen)
 Button(mgui, text='Submit', command=mgui.quit, padx=mgui.winfo_screenwidth()/2).grid(row=3, sticky=W, pady=4)
-app = FullScreenApp(mgui)
-mgui.mainloop()
+# Creating a button(tkinter instance, Button text, what function the button calls, location on screen)
+app = FullScreenApp(mgui)       # Calls function to make the gui full screen
+mgui.mainloop()     # Creates the GUI
+count(var1.get(), var2.get())
